3f4a18d339c9af220981ab30db7ce51180075165
foxxalpha <64504988+foxxalpha@users.noreply.github.com>
2020-09-09 20:56:37 -0400

    use Ternary operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,18 @@ import Form from "./Login";
 
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Form />;
-  }
-}
+// function renderConditionally() {
+//   if (isLoggedIn) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Form />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Form />}</div>
+  );
 }
 
 export default App;
